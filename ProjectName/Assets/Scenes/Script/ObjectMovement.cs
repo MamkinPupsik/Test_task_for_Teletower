ac6026b97ff3570a99a65a108b906cecf20ba173
dimidroll <JiJay.mr@mail.ru>
2023-08-13 00:06:38 +0600

    animation

--- ProjectName/Assets/Scenes/Script/ObjectMovement.cs
+++ ProjectName/Assets/Scenes/Script/ObjectMovement.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
 using UnityEngine.AI;
-
 public class ObjectMovement : MonoBehaviour
 {
-    public Transform[] points; // Массив точек перемещения
-    private int currentPointIndex = 0;
-    private NavMeshAgent agent;
-
+    [SerializeField] private Transform[] points; // Массив точек перемещения
+    [SerializeField] private int currentPointIndex = 0;
+    [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private bool check = false;
+    [SerializeField] private float increasedSpeed = 1f;
+    [SerializeField] private float startSpeed = 2.5f;
+    [SerializeField] private Animator anim;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         MoveToNextPoint();
+        agent.speed = startSpeed;
+        anim = GetComponent<Animator>();
     }
-
     private void Update()
-    {
-        // Проверяем, достиг ли объект текущей точки перемещения
+    {   
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
         {
             MoveToNextPoint();
@@ -24,9 +26,32 @@ public class ObjectMovement : MonoBehaviour
 
     private void MoveToNextPoint()
     {
-        // Устанавливаем новую цель перемещения
-        // Переходим к следующей точке или возвращаемся к первой, если достигли конца массива точек
+        
         agent.SetDestination(points[currentPointIndex].position);
         currentPointIndex = (currentPointIndex + 1) % points.Length;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerRun")) 
+        {
+            anim.SetBool("isRunning", true);
+            agent.speed += increasedSpeed; 
+        }
+
+        if (other.CompareTag("TriggerWalk")) 
+        {
+            if (agent.speed == startSpeed)
+            {
+                agent.speed = startSpeed;
+            }
+            else
+            {
+                anim.SetBool("isRunning", false);
+                agent.speed -= increasedSpeed;
+            }
+        }
+    }
+
+
 }
